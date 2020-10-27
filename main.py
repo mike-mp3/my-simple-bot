1a2655896b68593340803b284ccebc5df69a4490
mike-mp3 <mishamarkov12.0@gmail.com>
2020-10-27 17:40:59 +0300

    1drop

---
+++ main.py
@@ -0,0 +1,28 @@
+import telebot
+import config
+from random import *
+bot = telebot.TeleBot(config.TOKEN)
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('/Числа')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, чтобы начать выбери игру', reply_markup=keyboard1)
+
+
+@bot.message_handler(commands=['Числа'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я загадал число от одного до 5, напиши его в чат)')
+    global i
+    i: str = str(randint(0, 5))
+    print(i)
+    @bot.message_handler(content_types=['text'])
+    def send_text(message):
+        if message.text == i:
+            bot.send_message(message.chat.id, 'ОГо ты отгадал. Если хочешь продолжить тыкай на ИГРАТЬ')
+        elif message.text != i:
+            bot.send_message(message.chat.id, 'ТЫ не отгадал. Если хочешь продолжить тыкай на ИГРАТЬ')
+
+
+bot.polling(bot.polling(none_stop=True))
