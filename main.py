13c3bfd354585081cc777088e296061df03c22ce
mike-mp3 <mishamarkov12.0@gmail.com>
2020-11-01 20:28:22 +0300

    some changes, pep8

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import config
 from random import *
 bot = telebot.TeleBot(config.TOKEN)
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('/Числа')
+keyboard1.row('/Играть', '1', '2', '3', '4', '5')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет', reply_markup=keyboard1)
 
-@bot.message_handler(commands=['Числа'])
+@bot.message_handler(commands=['Играть'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Я загадал число от 1 до 5, напиши его в чат)😘')
     i: str = str(randint(1, 5))
-    print(i)
+    print("бот загадал:", i)
     @bot.message_handler(content_types=['text'])
     def send_text(message):
         if message.text == i:
